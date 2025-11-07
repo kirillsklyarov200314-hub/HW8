e69800002caffeda61200f9c020019f0c6f1477f
Kirill <kirill.sklyarov.200314@gmail.com>
2025-11-07 19:22:37 +0100

    Arrays

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -2,14 +2,29 @@ public class Main {
     public static void main(String[] args) {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
         //todo
+        int[] arr = new int[]{1,2,3,4,5,6,7,8,9,10};
 
         //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
         //todo
-
+        for(int i=0; i<arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        System.out.println("========================");
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
         //todo
-
+        for(int i=arr.length-1; i>=0; i--) {
+            System.out.println(arr[i]);
+        }
+        System.out.println("========================");
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
         //todo
+        for(int i=0; i<arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - (1 + i)];
+            arr[arr.length - (1 + i)] = temp;
+        }
+        for(int i=0; i<arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут
+        int[] arr = new int[10];
+        double sum = 0;
+
+        for(int i=0 ; i<arr.length; i++) {
+            arr[i] = (new Random().nextInt(61)) + 40;
+        }
+
+        int n=0;
+        for(int i=0 ; i<arr.length; i++) {
+            sum += arr[i];
+            if(arr[i] >= 60 && arr[i] <= 80) {
+                n++;
+                System.out.println("Макс" + arr[i]);
+            }
+        }
+
+        System.out.println("Средний вес: " + sum/arr.length);
+        System.out.println("Кол-во людей: " + n);
     }
 }
