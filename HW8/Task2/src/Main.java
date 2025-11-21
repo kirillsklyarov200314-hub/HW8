2381eca09f58e083bc2ddb8794e22f60103d4cfe
Kirill <kirill.sklyarov.200314@gmail.com>
2025-11-21 16:27:37 +0100

    Arrays

--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -3,23 +3,19 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут
-        int[] arr = new int[10];
-        double sum = 0;
+        double[] arr = new double[10];
+        double middleWeitght = 0;
+        int peopleCount=0;
 
         for(int i=0 ; i<arr.length; i++) {
-            arr[i] = (new Random().nextInt(61)) + 40;
-        }
-
-        int n=0;
-        for(int i=0 ; i<arr.length; i++) {
-            sum += arr[i];
+            arr[i] = (new Random().nextDouble() * 60) + 40;
+            middleWeitght += arr[i];
             if(arr[i] >= 60 && arr[i] <= 80) {
-                n++;
-                System.out.println("Макс" + arr[i]);
+                peopleCount++;
             }
         }
 
-        System.out.println("Средний вес: " + sum/arr.length);
-        System.out.println("Кол-во людей: " + n);
+        System.out.println("Средний вес: " + (middleWeitght / arr.length));
+        System.out.println("Кол-во людей: " + peopleCount);
     }
 }
